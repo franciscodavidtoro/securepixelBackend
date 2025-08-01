5c084bc1bb6befc2ae84b2fc711a70ab42118d47
David Toro <davidfranciscotoro@gmail.com>
2025-08-01 10:00:01 -0500

    .

--- ensennanza/urls.py
+++ ensennanza/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from .views import TemaListView,TemaDetailView
 
 urlpatterns = [
-    path('', TemaListView.as_view(), name='tema-list'),
-    path('<int:pk>/', TemaDetailView.as_view(), name='tema-detail'),
+    
 ]
