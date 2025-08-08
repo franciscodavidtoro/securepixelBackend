4c13955a9dbdcd9c1459c3e79bc48d1c5da4a133
David Toro <davidfranciscotoro@gmail.com>
2025-08-08 02:33:29 -0500

    /

--- AI/urls.py
+++ AI/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import AtencionAPIView, ProcesarImgEmocionesAPIView
 
 urlpatterns = [
-    path('atencion/', AtencionAPIView.as_view(), name='api-atencion'),
+    path('atencion', AtencionAPIView.as_view(), name='api-atencion'),
     path('emociones/<int:pk>/', ProcesarImgEmocionesAPIView.as_view(), name='api-emociones'),
 ]
