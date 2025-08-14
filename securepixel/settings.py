45764cfffdb6454996bf2029ed8826abd49760dd
David Toro <davidfranciscotoro@gmail.com>
2025-08-14 13:02:52 -0500

    kdkm

--- securepixel/settings.py
+++ securepixel/settings.py
@@ -132,6 +132,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
